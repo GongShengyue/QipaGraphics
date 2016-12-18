f6c862dfc75db197724fa6d87d08daab79a1246c
gsy <1007691667@qq.com>
2016-12-18 18:04:01 +0800

    第一个图形
    佛祖保佑！考试必过

---
+++ fozu.java
@@ -0,0 +1,42 @@
+﻿
+public class fozu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer();
+        sb.append("                   _ooOoo_\n");
+        sb.append("                  o8888888o\n");
+        sb.append("                  88\" . \"88\n");
+        sb.append("                  (| -_- |)\n");
+        sb.append("                  O\\  =  /O\n");
+        sb.append("               ____/`---'\\____\n");
+        sb.append("             .'  \\\\|     |//  `.\n");
+        sb.append("            /  \\\\|||  :  |||//  \\ \n");
+        sb.append("           /  _||||| -:- |||||-  \\ \n");
+        sb.append("           |   | \\\\\\  -  /// |   |\n");
+        sb.append("           | \\_|  ''\\---/''  |   |\n");
+        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
+        sb.append("         ___`. .'  /--.--\\  `. . __\n");
+        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        sb.append("                   `=---='\n");
+        sb.append("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+       /* sb.append("\t\t佛祖保佑       永无BUG\n");
+        sb.append("\t\t\t写字楼里写字间，写字间里程序员； \n");
+        sb.append("\t\t\t程序人员写程序，又拿程序换酒钱。\n");   
+        sb.append("\t\t\t酒醒只在网上坐，酒醉还来网下眠；\n");   
+        sb.append("\t\t\t酒醉酒醒日复日，网上网下年复年。\n");   
+        sb.append("\t\t\t但愿老死电脑间，不愿鞠躬老板前；\n");   
+        sb.append("\t\t\t奔驰宝马贵者趣，公交自行程序员。\n");   
+        sb.append("\t\t\t别人笑我忒疯癫，我笑自己命太贱；\n");   
+        sb.append("\t\t\t不见满街漂亮妹，哪个归得程序员？\n");
+*/      sb.append("\t\t佛祖保佑       考试必过\n");
+        System.out.println(sb.toString());
+	}
+
+}
